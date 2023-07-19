e3a6d0bc2472f400f076017f00556df1faa1a9b4
tm-moumakoe <123243154+tm-moumakoe@users.noreply.github.com>
2023-07-19 00:58:17 -0700

    Task 1 with standard library functions
    Co-authored-by: Tsumee <tsumeemoloisi@gmail.com>

---
+++ shell.c
@@ -0,0 +1,117 @@
+#include "shell.h"
+
+/**
+ * getcmd - reads the command using getline.
+ * @cmd: memory location of the command read.
+ * @len: the length parameter for getline.
+ *
+ * Return: the length of the input.
+ */
+
+ssize_t getcmd(char **cmd, size_t *len)
+{
+	ssize_t read_len;
+
+	if (isatty(fileno(stdin)))
+		write(STDOUT_FILENO, "#cisfun$ ", 9);
+
+	read_len = getline(cmd, len, stdin);
+	if (read_len == -1)
+	{
+		if (feof(stdin))
+		{
+			free(*cmd);
+			exit(EXIT_SUCCESS);
+		}
+		perror("getline");
+		free(*cmd);
+		exit(EXIT_FAILURE);
+	}
+
+	if (read_len > 0 && (*cmd)[read_len - 1] == '\n')
+		(*cmd)[read_len - 1] = '\0';
+
+	return (read_len);
+}
+
+/**
+ * execute - forks a child process and executes the command using execve.
+ * @cmd: the command to execute.
+ * @av: argument vector from main containing the name of the program.
+ * @env: the list of environment variables.
+ *
+ * Return: the child pid on success or -1 on failure.
+ */
+
+int execute(char *cmd, char **av, char **env)
+{
+	int status;
+	pid_t pid;
+	char **exec = malloc(2 * sizeof(char *));
+
+	exec[0] = malloc(MAX_COM_LEN * sizeof(char));
+	strcpy(exec[0], cmd);
+
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		free(exec[0]);
+		free(exec);
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(exec[0], exec, env) == -1)
+		{
+			perror(av[0]);
+			free(exec[0]);
+			free(exec);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		if (waitpid(pid, &status, 0) == -1)
+		{
+			perror("waitpid");
+			free(exec[0]);
+			free(exec);
+			exit(EXIT_FAILURE);
+		}
+
+	free(exec[0]);
+	free(exec);
+	return (status);
+}
+
+/**
+ * main - UNIX command line interpreter.
+ * @ac: arg count.
+ * @av: arg vector.
+ * @env: list of env vars.
+ *
+ * Return: 0 (Success).
+ */
+
+int main(int ac __attribute__((unused)), char **av, char **env)
+{
+	char *command = NULL;
+	size_t len = 0;
+	ssize_t read_len;
+	int status;
+	bool interactive = isatty(fileno(stdin));
+
+	while (1)
+	{
+		read_len = getcmd(&command, &len);
+		if (!interactive && read_len <= 1)
+			break;
+
+		status = execute(command, av, env);
+		if (status == -1)
+			exit(EXIT_FAILURE);
+	}
+
+	free(command);
+	return (0);
+}
---
+++ shell.h
@@ -0,0 +1,33 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <stdbool.h>
+
+#define MAX_COM_LEN 128
+#define MAX_TOK_LEN 64
+
+int _putchar(char c);
+char *_strcpy(char *dest, char *src);
+int split(char *str, const char *delimiters, char **tokens, int max_tokens);
+void _puts(char *str);
+char *_strdup(const char *s);
+size_t _strlen(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strchr(char *s, char c);
+char *_strcat(char *dest, char *src);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _isalpha(int c);
+int _atoi(char *s);
+
+#endif
