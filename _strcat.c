288a0e43ff2efb8d9e0277b80964527d35432fce
Tsumee <tsumeemoloisi@gmail.com>
2023-07-26 23:20:24 -0700

    betty mod

--- _strcat.c
+++ _strcat.c
@@ -2,8 +2,8 @@
 
 /**
  * _strcat - Concatenates (put together) the string pointed to by @src,
- *           to the end of the string pointed to by @dest, terminating null byte
- *           at the end.
+ *           to the end of the string pointed to by @dest,
+ *           terminating null byte at the end.
  * @dest: pointer to the string to be concatenated upon.
  * @src: pointer to source string to be appended to @dest.
  *
