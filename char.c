fdef2ed788ef64d89ce80e720482a77941f3218e
Tsumee <tsumeemoloisi@gmail.com>
2023-07-28 02:02:55 -0700

    file organization

---
+++ char.c
@@ -0,0 +1,91 @@
+#include "shell.h"
+
+/**
+ * is_delim - checks if a char is a delimiter.
+ * @c: the char to check.
+ * @delim: string of delimiters.
+ *
+ * Return: true if delim, false if not.
+ */
+
+bool is_delim(char c, const char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return(true);
+	return (false);
+}
+
+/**
+ * _isalpha - checks for alphabetic char.
+ * @c: input to _isalpha.
+ *
+ * Return: true if c is alphabetic, false otherwise.
+ */
+
+bool _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
+
+/**
+ * _putchar - writes a char to stdout.
+ * @c: char to print.
+ *
+ * Return: 1 on success, -1 on error and errno is set appropriately.
+ */
+
+int _putchar(int c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * _putfd - writes a char to a given file descriptor.
+ * @c: char to write.
+ * @fd: file descriptor to write to.
+ *
+ * Return: 1 on success, -1 on error amd errno is set appropriately.
+ */
+
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ * rmcmt - replaces first instance of '#' with '\0'.
+ * @buf: address of string to modify.
+ */
+
+void rmcmt(char *buf)
+{
+	int i = 0;
+
+	for (; buf[i]; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			return;
+		}
+}
---
+++ int.c
@@ -0,0 +1,79 @@
+#include "shell.h"
+
+/**
+ * _atoi - function converts string to an integer
+ * @s: Pointer parameter
+ * Return: returns an integer value
+ */
+
+int _atoi(char *s)
+{
+	unsigned int count = 0, size = 0, result = 0, sign = 1, m = 1, i;
+
+	while (*(s + count) != '\0')
+	{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
+			break;
+
+		if (*(s + count) == '-')
+			sign *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
+		}
+		count++;
+	}
+
+	i = count - size;
+	while (i < size && m >= 1); 
+	{
+		result = result + ((*(s + i) - 48) * m);
+		m /= 10;
+		i++;
+	}
+	return (result * sign);
+}
+
+/**
+ * print_d - prints an int to a file descriptor.
+ * @num: the input.
+ * @fd: file descriptor to write to.
+ *
+ * Return: number of chars printed.
+ */
+
+int print_d(int num, int fd)
+{
+	int i, count = 0, (*p_putchar)(char) = _putchar;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		p_putchar = _eputchar;
+	if (num < 0)
+	{
+		_abs_ = -num;
+		p_putchar('-');
+		count++;
+	}
+	else
+		_abs_ = num;
+	current = _abs_;
+	i = 1000000000;
+	while (i > 1)
+	{
+		if (_abs_ / i)
+		{
+			p_putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+		i /= 10;
+	}
+	p_putchar('0' + current);
+	count++;
+
+	return (count);
+}
